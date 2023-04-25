2937378d8498eade2a73be904e59e7bf15575263
Filipe Lima <filiperopecalima@gmail.com>
2023-04-25 11:30:31 -0300

    Resolução desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,43 +15,27 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
+            if (hospedes.Count <= Suite.Capacidade){
                 Hospedes = hospedes;
             }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+            else{
+                throw new Exception("Quantidade de hospedes excede a capacidade da suíte");
             }
         }
 
-        public void CadastrarSuite(Suite suite)
-        {
+        public void CadastrarSuite(Suite suite){
             Suite = suite;
         }
 
-        public int ObterQuantidadeHospedes()
-        {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+        public int ObterQuantidadeHospedes(){
+            return Hospedes.Count;
         }
 
-        public decimal CalcularValorDiaria()
-        {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
+        public decimal CalcularValorDiaria(){
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
+            if (DiasReservados >= 10){
+                valor = valor * 0.9M;
             }
 
             return valor;
